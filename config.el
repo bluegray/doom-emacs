b6795e3401c648fda3752b65499a55a04d472743
Francois du Toit <bluegray@users.noreply.github.com>
2023-05-30 12:10:33 +0200

    Cleanup

--- config.el
+++ config.el
@@ -5,23 +5,86 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
+;; clients, file templates and snippets. It is optional.
 (setq user-full-name "John Doe"
       user-mail-address "john@doe.com")
 
-(defvar ui-hidpi t "Set hidpi settings and fonts")
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;;(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Custom doom configuration ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Fonts
+
+(defvar ui-hidpi t "Check env to maybe set hidpi settings and fonts")
 (setq ui-hidpi (string= (getenv "COMPUTER") "mbp"))
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
 (setq
  doom-font
  (font-spec :family (if ui-hidpi "Ubuntu Mono" "ProggyCleanTTSZ")
@@ -32,46 +95,8 @@
  doom-big-font
  (font-spec :family (if ui-hidpi "Ubuntu Mono" "Source Code Pro")
             :size   (if ui-hidpi 30 16)))
-(after! doom-themes
-  (setq doom-themes-enable-bold t))
 
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-;;(setq doom-theme 'doom-one)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package' for configuring packages
-;; - `after!' for running code after a package has loaded
-;; - `add-load-path!' for adding directories to the `load-path', relative to
-;;   this file. Emacs searches the `load-path' when you load packages with
-;;   `require' or `use-package'.
-;; - `map!' for binding new keys
-;;
-;; To get information about any of these functions/macros, move the cursor over
-;; the highlighted symbol at press 'K' (non-evil users must press 'C-c g k').
-;; This will open documentation for it, including demos of how they are used.
-;;
-;; You can also try 'gd' (or 'C-c g d') to jump to their definition and see how
-;; they are implemented.
-
-
-;; Custom doom configuration
-
-(add-to-list 'default-frame-alist '(fullscreen . maximized))
-(add-to-list 'custom-theme-load-path "~/.doom.d/themes/")
+;; Whitespace
 
 (setq whitespace-style
       '(face tabs tab-mark spaces space-mark trailing missing-newline-at-eof lines-tail empty))
@@ -82,19 +107,19 @@
 ;; Set whitespace characters here:
 ;; All numbers are unicode codepoint in decimal. e.g. (insert-char 182 1)
 (setq whitespace-display-mappings
-      '((space-mark 32 [183] [46]) ; SPACE 32 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
-        (newline-mark 10 [182 10]) ; LINE FEED,
-        (tab-mark 9 [155 9] [92 9])     ; tab
+      '((space-mark 32 [183] [46])  ; SPACE 32 「 」, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+        (newline-mark 10 [182 10])  ; LINE FEED,
+        (tab-mark 9 [155 9] [92 9]) ; tab
         ))
 
-;;(setq scroll-conservatively 101) ;; move minimum when cursor exits view, instead of recentering
-(setq mouse-wheel-scroll-amount '(1)) ;; mouse scroll moves 1 line at a time, instead of 5 lines
-(setq mouse-wheel-progressive-speed 10) ;; on a long mouse scroll keep scrolling by 1 line
+;; Theme
+
+(add-to-list 'custom-theme-load-path "~/.doom.d/themes/")
 
 (use-package! doom-themes
   :config
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (setq doom-themes-enable-bold t       ; if nil, bold is universally disabled
+        doom-themes-enable-italic t)    ; if nil, italics is universally disabled
   ;;(load-theme 'doom-city-lights t)
   ;;(load-theme 'doom-nord t)
   ;;(load-theme 'doom-spacegrey t)
@@ -106,6 +131,14 @@
   (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
   (doom-themes-treemacs-config)
   (doom-themes-org-config))
+
+;;Misc
+
+;;(setq scroll-conservatively 101) ;; move minimum when cursor exits view, instead of recentering
+(setq mouse-wheel-scroll-amount '(1)) ;; mouse scroll moves 1 line at a time, instead of 5 lines
+(setq mouse-wheel-progressive-speed 10) ;; on a long mouse scroll keep scrolling by 1 line
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
 
 
 ;;;;;;;;;;;;;;;;;;;
